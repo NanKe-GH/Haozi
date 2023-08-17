c73f889d97623352caafd83f129235a7e7901d4f
NanKe-GH <2524845627@qq.com>
2023-08-17 19:01:57 +0800

    lazyload

--- script.js
+++ script.js
@@ -20,3 +20,43 @@ menuButtons.forEach(button => {
         document.getElementById(targetContent).style.display = 'block';
     });
 });
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    let lazyImages = document.querySelectorAll("img[data-src]");
+  
+    function lazyLoad() {
+      lazyImages.forEach(function(img) {
+        if (img.getBoundingClientRect().top <= window.innerHeight && img.getBoundingClientRect().bottom >= 0 && getComputedStyle(img).display !== "none") {
+          img.src = img.dataset.src;
+          img.removeAttribute("data-src");
+        }
+      });
+  
+      lazyImages = Array.prototype.filter.call(lazyImages, function(img) {
+        return img.getAttribute("data-src");
+      });
+    }
+  
+    lazyLoad();
+  
+    if ("IntersectionObserver" in window) {
+      let lazyImageObserver = new IntersectionObserver(function(entries) {
+        entries.forEach(function(entry) {
+          if (entry.isIntersecting) {
+            let lazyImage = entry.target;
+            lazyImage.src = lazyImage.dataset.src;
+            lazyImage.removeAttribute("data-src");
+            lazyImageObserver.unobserve(lazyImage);
+          }
+        });
+      });
+  
+      lazyImages.forEach(function(lazyImage) {
+        lazyImageObserver.observe(lazyImage);
+      });
+    } else {
+      window.addEventListener("scroll", lazyLoad);
+    }
+  });
+  
